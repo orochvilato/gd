279e1f75378aea64bc97fab7bb8851c7fd0a6d14
Olivier ROCH VILATO <olivier.rochvilato@chilbp.fr>
2017-11-14 12:03:17 -0400

    logs

--- gd/views/logs.py
+++ gd/views/logs.py
@@ -1,10 +1,10 @@
-from obsapis import app,use_cache,mdb
+from gd import app,use_cache,mdb
 from flask import request,current_app,make_response
-from obsapis.tools import json_response,cache_function
+from gd.tools import json_response,cache_function
 import re
 import datetime
 
-from obsapis.config import cache_pages_delay
+from gd.config import cache_pages_delay
 
 @app.route('/logs')
 def logs():
